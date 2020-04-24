6fcd3180ae7e125a6b7b018fe09518053be58877
Gustavo doll <gustavo.doll@iteris.com.br>
2020-04-24 13:56:12 -0300

    initilal commitl commit with firebase implements

---
+++ Notification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification
+//
+//  Created by Gustavo Henrique Souza Silva Doll - GDO on 17/04/20.
+//  Copyright © 2020 Gustavo Henrique Souza Silva Doll - GDO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
